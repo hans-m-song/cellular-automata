bb7d72e499e6be0ffe53dcd09d0ff548975e7ad9
Hans Song <hans.m.song@gmail.com>
2020-08-28 12:47:17 +1000

    reorder and rename tests

--- test.sh
+++ test.sh
@@ -1,13 +1,13 @@
 #/bin/bash
 
-dimensions=(10 20 30 40 50 60 70 80 90 100 200 300 400 500 600 700 800 900 1000)
+count=(10 20 30 40 50 60 70 80 90 100 200 300 400 500 600 700 800 900 1000 10000)
 
-for dimension in ${dimensions[@]}; do 
-  echo $dimension 100
-  ./cellular_automata $dimension $dimension 0.5 100 >> dimension.output.csv
-done
-
-for iteration in ${dimensions[@]}; do
+for iteration in ${count[@]}; do
   echo 100 $iteration
   ./cellular_automata 100 100 0.5 $iteration >> iteration.output.csv
+done
+
+for dimension in ${count[@]}; do 
+  echo $dimension 100
+  ./cellular_automata $dimension $dimension 0.5 100 >> dimension.output.csv
 done
