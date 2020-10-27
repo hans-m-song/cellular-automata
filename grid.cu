3e3fc65bf37b4e6279ab8e4c72b04b2999035a46
Hans Song <hans.m.song@gmail.com>
2020-10-28 02:27:25 +1000

    cuda makefile and working compile

--- grid.cu
+++ grid.cu
@@ -10,36 +10,60 @@
 #include "metric.hpp"
 #include "util.hpp"
 
-int width;
-int height;
-int size;
-int* generation;
-int* d_generation
-int* next_generation;
-int* d_next_generation
 Metric metric;
+int* generation;
+int* next_generation;
+
+int* d_generation;
+int* d_next_generation;
+
+int h_width;
+int h_height;
+
+__device__ int width;
+__device__ int height;
 
 void checkError(cudaError_t e) {
-   if (e != cudaSuccess) {
-      std::cerr << "CUDA error: " << int(e) << ": " 
-                << cudaGetErrorString(e) << "\n";
-      abort();
-   }
+  if (e != cudaSuccess) {
+    std::cerr << "CUDA error: " << int(e) << ": " 
+              << cudaGetErrorString(e) << "\n";
+    abort();
+  }
 }
 
-void init(double initial_density) {
-  metric.start(Measure::Total);
-  metric.start(Measure::Init);
+int empty_cell(int* generation, int width, int height) {
+  int x, y, point;
+  for (int i = 0; i < width * height; i++) {
+    x = random(0, width - 1);
+    y = random(0, height - 1);
+    point = x * width + y;
+    if (!generation[point]) {
+      return point;
+    }
+  }
+
+  return 0;
+}
+
+void init(int width, int height, double initial_density) {
+  checkError(cudaMemcpyToSymbol("width", &width, sizeof(int)));
+  checkError(cudaMemcpyToSymbol("height", &height, sizeof(int)));
+
+  int size = width * height;
+
+  generation = new int[size];
+  next_generation = new int[size];
   
-  generation = allocate_space();
-  next_generation = allocate_space();
+  for (int i = 0; i < size; i++) {
+    generation[i] = 0;
+  }
   
   int initial_cells = (int)(initial_density * size);
   
-  Point point;
+  int point;
   for (int i = 0; i < initial_cells; i++) {
-    point = empty_cell();
-    generation[point.first * width + point.second] = 1;
+    point = empty_cell(generation, width, height);
+    generation[point] = 1;
   }
   
   // allocate memory on device
@@ -48,9 +72,7 @@ void init(double initial_density) {
 
   // copy over empty data
   checkError(cudaMemcpy(d_generation, generation, size, cudaMemcpyHostToDevice));
-  checkError(cudaMemcpy(d_next_generation, next_generation, size, cudaMemcpyHostToDevice));
-
-  metric.stop(Measure::Init);
+  // don't need to initialize next_generation since it'll be overwritten during tick
 }
 
 void deinit() {
@@ -60,96 +82,10 @@ void deinit() {
   cudaFree(d_next_generation);
 }
 
-int* allocate_space(void) {
-  int* space = new int[size];
-  for (int x = 0; x < width; x++) {
-    for (int y = 0; y < height; y++) {
-      space[x * width + y] = 0;
-    }
-  }
-  return space;
-}
-
-
-Point empty_cell(void) {
-  int x, y;
-  for (int i = 0; i < size; i++) {
-    x = random(0, width - 1);
-    y = random(0, height - 1);
-    if (!generation[x * width + y]) {
-      return Point(x, y);
-    }
-  }
-
-  return Point(-1, -1);
-}
-
 __device__
-int sum_neighbour(int x, int y) {
-  int sum = 0;
-  Point origin = Point(x, y);
-  Point adjacent;
-  // check each direction
-  for (int i = Direction::N; i <= Direction::NW; i++) {
-    adjacent = apply_direction(origin, (Direction)i);
-    if (d_generation[adjacent.first * width + adjacent.second]) {
-      sum += 1;
-    }
-  }
-  return sum;
-}
-
-void print(void) {
-#ifndef DEBUG
-  std::cout << "\e[1;1H\e[2J";
-#endif
-
-  for (int x = 0; x < width; x++) {
-    for (int y = 0; y < height; y++) {
-      std::cout << (d_generation[x * width + y] ? "o " : ". ");
-    }
-    std::cout << "\n";
-  }
-}
-
-__global__
-void run(int ticks) {
-  for (int i = 0; i < ticks; i++) {
-    __syncthreads(); // ensure generation is completely generated
-    tick();
-  }
-}
-
-__global__
-void tick(void) {
-  int x = blockIdx.x * blockDim.x + threadIdx.x;
-  int y = blockIdx.y * blockDim.y + threadIdx.y;
-  int x_stride = blockDim.x * gridDim.x;
-  int y_stride = blockDim.y * gridDim.y;
-
-  for (int i = x; i < N; i += x_stride) {
-    for (int j = y; j < N; j += y_stride) {
-      int neighbours = sum_neighbour(i, j);
-      if (neighbours == 3) {
-        // if 3 live neighbours exactly, cell lives
-        d_next_generation[i * width + j] = 1;
-      } else if (neighbours == 2) {
-        // if 2 live neighbours exactly, cell maintains status
-        d_next_generation[i * width + j] = d_generation[i * width + j];
-      } else {
-        // otherwise else cell dies
-        d_next_generation[i * width + j] = 0;
-      }
-    }
-  }
-  // swap currently active generation
-  std::swap(generation, next_generation);
-}
-
-__device__
-Point apply_direction(Point origin, Direction direction) {
-  int x = origin.first;
-  int y = origin.second;
+int apply_direction(int i, int j, Direction direction) {
+  int x = i;
+  int y = j;
   switch (direction) {
     case Direction::N:
       x += 1;
@@ -181,21 +117,87 @@ Point apply_direction(Point origin, Direction direction) {
       break;
   }
 
-  return Point((x + width) % width, (y + height) % height);
+  return ((x + width) % width) * width + ((y + height) % height);
 }
 
-Metric run_grid(int h_width, int h_height, double initial_density, int ticks) {
-  width = h_width;
-  height = h_height;
-  size = width * height;
+__device__
+int sum_neighbour(int* d_generation, int x, int y) {
+  int sum = 0;
+  int adjacent;
+  // check each direction
+  for (int i = Direction::N; i <= Direction::NW; i++) {
+    adjacent = apply_direction(x, y, (Direction)i);
+    if (d_generation[adjacent]) {
+      sum += 1;
+    }
+  }
+  return sum;
+}
 
-  init(initial_density);
+__device__
+void tick(int* d_generation, int* d_next_generation) {
+  int x = blockIdx.x * blockDim.x + threadIdx.x;
+  int y = blockIdx.y * blockDim.y + threadIdx.y;
+  int x_stride = blockDim.x * gridDim.x;
+  int y_stride = blockDim.y * gridDim.y;
+
+  for (int i = x; i < N; i += x_stride) {
+    for (int j = y; j < N; j += y_stride) {
+      int neighbours = sum_neighbour(d_generation, i, j);
+      if (neighbours == 3) {
+        // if 3 live neighbours exactly, cell lives
+        d_next_generation[i * width + j] = 1;
+      } else if (neighbours == 2) {
+        // if 2 live neighbours exactly, cell maintains status
+        d_next_generation[i * width + j] = d_generation[i * width + j];
+      } else {
+        // otherwise else cell dies
+        d_next_generation[i * width + j] = 0;
+      }
+    }
+  }
+  // swap currently active generation
+  auto temp = d_generation;
+  d_generation = d_next_generation;
+  d_next_generation = temp;
+}
+
+__global__
+void run(int ticks, int* d_generation, int* d_next_generation) {
+  for (int i = 0; i < ticks; i++) {
+    __syncthreads(); // ensure generation is completely generated
+    tick(d_generation, d_next_generation);
+  }
+}
+
+void print(void) {
+#ifndef DEBUG
+  std::cout << "\e[1;1H\e[2J";
+#endif
+
+  for (int x = 0; x < h_width; x++) {
+    for (int y = 0; y < h_height; y++) {
+      std::cout << (d_generation[x * h_width + y] ? "o " : ". ");
+    }
+    std::cout << "\n";
+  }
+}
+
+Metric run_grid(int width, int height, double initial_density, int ticks) {
+  h_width = width;
+  h_height = height;
+  metric.start(Measure::Total);
+
+  metric.start(Measure::Init);
+  init(width, height, initial_density);
+  metric.stop(Measure::Init);
 
   metric.start(Measure::Run);
-  run<<<1, 1>>>(ticks);
+  run<<<1, 1>>>(ticks, d_generation, d_next_generation);
   metric.stop(Measure::Run);
-  metric.stop(Measure::Total);
   
+  metric.stop(Measure::Total);
+
   deinit();
 
   return metric;
--- makefile
+++ makefile
@@ -1,6 +1,8 @@
 CXX				= g++
-CXXFLAGS 	= -Wall -std=c++1y $(DEFINE)
-OBJ 			= metric.o grid.o util.o main.o
+CXXFLAGS 	= $(DEFINE) -Wall -std=c++1y -O2
+OBJ 			= metric.o util.o main.o
+NVCC			= nvcc
+NVFLAGS		= $(DEFINE) -O2 --gpu-architecture=sm_35 -Wno-deprecated-gpu-targets
 
 %.o: %.cpp
 	$(CXX) $(CXXFLAGS) -c -o $@ $<
@@ -9,8 +11,11 @@ all: main
 
 new: clean main
 
-main: $(OBJ)
-	$(CXX) $(CXXFLAGS) -o cellular_automata $(OBJ)
+main: $(OBJ) grid.o
+	$(CXX) $(CXXFLAGS) -o cellular_automata $(OBJ) grid.o
+
+cuda: $(OBJ)
+	$(NVCC) $(NVFLAGS) -o cuda_cellular_automata $(OBJ) grid.cu
 
 visual: CXXFLAGS += -DVISUAL
 visual: new
